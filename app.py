9d5308fc83794c0d0fbbaefec912b45eee5f0e24
adisonoptic77-glitch <adisonoptic77@gmail.com>
2026-04-22 10:01:16 -0600

    cuarto comentario

--- app.py
+++ app.py
@@ -2,3 +2,16 @@ print("Hola mi nombre es adison")
 print("naci el 24 de agosto de 2007")
 print("calle capitan antonio fernandez becerra, san diego")
 print("18")
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route("/saludo", methods=["GET"])
+def home():
+    return jsonify({"mensaje": "API funcionando"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
